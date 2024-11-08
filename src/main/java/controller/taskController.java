d56760c93ebb1efaed9042c91f0d423ab9a1ba8f
abdulwariskhan <hellokhan.ch@gmail.com>
2024-11-08 08:10:41 +0100

    injected taskifyService to task controller class

--- src/main/java/controller/taskController.java
+++ src/main/java/controller/taskController.java
@@ -1,4 +1,15 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import service.taskifyService;
+
+@Controller
 public class taskController {
+
+    private final taskifyService service;
+    @Autowired
+    public taskController(taskifyService service) {
+        this.service = service;
+    }
 }
