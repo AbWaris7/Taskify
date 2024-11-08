88c46612a2f4f1d429cd22b28df18d534e9978cc
abdulwariskhan <hellokhan.ch@gmail.com>
2024-11-08 20:02:20 +0100

    Setting up Controller

--- src/main/java/controller/taskController.java
+++ src/main/java/controller/taskController.java
@@ -1,8 +1,13 @@
 package controller;
 
+import model.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import service.taskifyService;
+
+import java.util.List;
 
 @Controller
 public class taskController {
@@ -11,5 +16,14 @@ public class taskController {
     @Autowired
     public taskController(taskifyService service) {
         this.service = service;
+    }
+
+    @GetMapping()
+    public String getTasks(Model model) {
+
+        List<Task> tasks = service.getAllTasks();
+        model.addAttribute("tasks", tasks);
+
+        return "tasks";
     }
 }
