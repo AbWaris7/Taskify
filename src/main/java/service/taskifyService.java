d8983f6b494757c1ab2a75dffad0127d53c6cd1a
abdulwariskhan <hellokhan.ch@gmail.com>
2024-11-08 20:02:30 +0100

    Setting up Service

--- src/main/java/service/taskifyService.java
+++ src/main/java/service/taskifyService.java
@@ -1,7 +1,24 @@
 package service;
 
+import model.Task;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import repository.TaskRepository;
+
+import java.util.List;
 
 @Service
 public class taskifyService {
+
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public taskifyService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+
+        return taskRepository.findAll();
+    }
 }
